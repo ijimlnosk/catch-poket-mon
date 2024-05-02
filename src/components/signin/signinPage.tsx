6f2dfb266243cd2a10ae798e2de7427f28305c55
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-03 02:11:01 +0900

    design/signin 수정

--- src/components/signin/signinPage.tsx
+++ src/components/signin/signinPage.tsx
@@ -38,17 +38,17 @@ const SigninPage: React.FC = () => {
             {/* 빨간색 섹션 */}
             <div
                 ref={transitionRef}
-                className={`w-full h-1/2 bg-COMMON-red flex flex-col justify-end items-center overflow-hidden transition-all duration-500 ease-in-out  ${
+                className={`w-full h-1/2 bg-COMMON-red flex flex-col justify-end items-center overflow-hidden transition-all duration-500 ease-in-out z-50 ${
                     isOpen
                         ? "transform translate-y-0"
                         : "transform -translate-y-3/4"
                 }`}
             >
                 <div className="flex w-full">
-                    <div className="w-2/5 flex flex-col justify-end items-center">
+                    <div className="w-[45%] flex flex-col justify-end items-center">
                         <div className="w-full h-4 bg-SYSTEM-black "></div>
                     </div>
-                    <div className="w-80 h-40  bg-SYSTEM-black rounded-t-full flex flex-col justify-end items-center">
+                    <div className="min-w-[310px] h-[160px]  bg-SYSTEM-black rounded-t-full flex flex-col justify-end items-center">
                         <div className="w-60 h-32 bg-SYSTEM-white rounded-t-full flex flex-col justify-end items-center">
                             <button
                                 onClick={handleSections}
@@ -56,19 +56,19 @@ const SigninPage: React.FC = () => {
                             ></button>
                         </div>
                     </div>
-                    <div className="w-2/5 flex flex-col justify-end items-center">
+                    <div className="w-[45%] flex flex-col justify-end items-center">
                         <div className="w-full h-4 bg-SYSTEM-black "></div>
                     </div>
                 </div>
             </div>
 
             <div
-                className={`flex items-center justify-center transition-opacity duration-500 ease-in-out ${
-                    showLoginForm ? "" : "hidden"
+                className={`flex items-center justify-center transition-opacity duration-500 ease-in-out absolute translate-x-[-50%] translate-y-[-50%] left-1/2 top-1/2 ${
+                    showLoginForm ? "opacity-100" : "opacity-0"
                 }`}
             >
                 {/* loginForm */}
-                <div className="w-1/3 flex items-center justify-center flex-col bg-SYSTEM-white pb-20 rounded-xl">
+                <div className="w-[400px]  flex items-center justify-center flex-col pb-20 rounded-xl">
                     <div>
                         <img src={logo} />
                     </div>
@@ -83,6 +83,14 @@ const SigninPage: React.FC = () => {
                             placeholder="비밀번호를 입력하세요"
                             className="border-2 border-COMMON-light-gray p-2"
                         />
+                        <button className="p-4">로그인</button>
+                    </div>
+
+                    <div className="flex flex-row">
+                        <p className="p-4 text-ti text-POKETYPE-rock">
+                            아직도 회원이 아니신가요?
+                        </p>
+                        <button className="text-ti">가입하러가기</button>
                     </div>
                 </div>
             </div>
@@ -96,13 +104,13 @@ const SigninPage: React.FC = () => {
                 }`}
             >
                 <div className="flex w-full">
-                    <div className="w-2/5">
+                    <div className="w-[45%]">
                         <div className="w-full h-4 bg-SYSTEM-black"></div>
                     </div>
-                    <div className="w-80 h-40 bg-SYSTEM-black rounded-b-full flex flex-col justify-start items-center">
+                    <div className="min-w-[310px] h-[160px] bg-SYSTEM-black rounded-b-full flex flex-col justify-start items-center">
                         <div className="w-60 h-32 bg-SYSTEM-white rounded-b-full"></div>
                     </div>
-                    <div className="w-2/5">
+                    <div className="w-[45%]">
                         <div className="w-full h-4 bg-SYSTEM-black "></div>
                     </div>
                 </div>
