c04f1a597c0d766eb111d1330ce90ca4dc90e57d
hayoung <wendy4332@naver.com>
2024-05-02 23:20:15 +0900

    feat:designToken설정 및 폰트 추가설정

---
+++ src/libs/tailwindCss/designToken/color.js
@@ -0,0 +1,24 @@
+export const COLORS = {
+    SYSTEM: {
+        red: "#D93E30",
+        black: "#212121",
+        white: "#FBFBFB",
+    },
+    MAIN: {
+        yellow: "#FFF9C0",
+        gray: "#D9D9D9",
+    },
+    COMMON: {
+        red: "#C5312A",
+    },
+    POKETYPE: {
+        rock: "#A1A1A1",
+        grass: "#70A83B",
+        ice: "#A2CFF0",
+        fire: "#F76545",
+        normal: "#76AADB",
+        poison: "#A974BC",
+        ground: "#9B897B",
+        electric: "#F7C545",
+    },
+};
---
+++ src/libs/tailwindCss/designToken/fontSize.js
@@ -0,0 +1,8 @@
+export const FONT_SIZE= {
+	xl: "32px", // extreme large
+	lg: "24px", // large
+	bg: "18px", // big
+	md: "15px", // medium
+	sm: "13px", // small
+	ti: "11px", // tiny
+};
