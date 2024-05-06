043c2791d31220c62bca116549a1d469da21df99
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-06 16:39:25 +0900

    feat/loadingPage 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 import { COLORS } from "./src/libs/tailwindCss/designToken/color";
 import { FONT_SIZE } from "./src/libs/tailwindCss/designToken/fontSize";
 export default {
+    mode: "jit",
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
@@ -10,6 +11,15 @@ export default {
             },
             fontSize: {
                 ...FONT_SIZE,
+            },
+            keyframes: {
+                roll: {
+                    "0%": { transform: "rotate(0deg)" },
+                    "100%": { transform: "rotate(360deg)" },
+                },
+            },
+            animation: {
+                roll: "roll 2s linear infinite",
             },
         },
         fontFamily: {
