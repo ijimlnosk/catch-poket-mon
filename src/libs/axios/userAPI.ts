35ce224d68603b4c186e436d20c6cc4f04d27205
hayoung <wendy4332@naver.com>
2024-05-07 12:45:18 +0900

    feat:userApi 생성완료

---
+++ src/libs/axios/userAPI.ts
@@ -0,0 +1,67 @@
+
+import { profile } from "console";
+import { userDataInstance } from "./axiosInstance";
+
+
+export interface UserRuest {
+    userId:string;
+    password:string;
+    nickname?:string;
+}
+
+//회원가입
+export const postSignup = async({userId, password,nickname} :UserRuest) =>{
+    const response = await userDataInstance.post("/user/sign-up", {
+        userId,
+        password,
+        nickname
+    })
+    return response
+}
+
+//로그인
+export const postSignin = async({userId,password}:UserRuest)=> {
+const response = await userDataInstance.post("/user/sign-in",{
+    userId,
+    password,
+})
+return response
+}
+
+//로그아웃
+export const postSignout = async()=>{
+    const response=await userDataInstance.post("/user/sign-out")
+    return response
+}
+
+//리프레쉬토큰
+export const getRefresh = async()=> {
+    const response =await userDataInstance.get("/user/refresh")
+    if(response.status === 200 ){
+        return response.data.token
+    } else {
+        console.error("접근 권한이 없습니다")
+    }
+}
+
+//회원정보 수정
+interface profile {
+image?: string;
+nickname?:string
+}
+//프로필이미지 수정
+export const patchUpdateProfileUrl = async({image}:profile)=>{
+    const response = await userDataInstance.patch("/user/update/profileUrl",{
+        image
+    })
+    return response
+
+}
+
+//프로필 닉네임 수정
+export const patchUpdateInfo = async({nickname}:profile)=>{
+    const response =await userDataInstance.patch("/user/update.info",{
+        nickname
+    })
+    return response
+}
