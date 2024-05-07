da596709c2e019cb2dbbde3dc4882531cfaa3667
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-07 18:04:40 +0900

    react-query app.tsx에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import router from "./libs/router/router";
 import { store } from "./libs/redux/store";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <div className="font-DungGeunMo">
-            <Provider store={store}>
-                <RouterProvider router={router}></RouterProvider>
-            </Provider>
+            <QueryClientProvider client={queryClient}>
+                <Provider store={store}>
+                    <RouterProvider router={router}></RouterProvider>
+                </Provider>
+            </QueryClientProvider>
         </div>
     );
 }
