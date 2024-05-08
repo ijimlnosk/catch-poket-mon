f1d06c0100a8c155d9a1e12fb07757bf90c9c4c0
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-09 03:55:19 +0900

    baseUrl 수정

--- src/libs/axios/axiosInstance.ts
+++ src/libs/axios/axiosInstance.ts
@@ -2,19 +2,22 @@ import axios from "axios";
 import { getSessionToken } from "../../utils/storageUtils";
 
 export const pocketmonInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_POKETMON_BASE_URL}`,
+    baseURL: `${import.meta.env.VITE_APP_POKE_BASE_URL}`,
 });
 
 export const userDataInstance = axios.create({
-    baseURL:`${import.meta.env.VITE_APP_URL}`
-})
+    baseURL: `${import.meta.env.VITE_APP_URL}`,
+});
 
-userDataInstance.interceptors.request.use((authData)=>{
-    const token = getSessionToken()
-    if(token) {
-        authData.headers["Authorization"]=`Bearer ${token}`
+userDataInstance.interceptors.request.use(
+    (authData) => {
+        const token = getSessionToken();
+        if (token) {
+            authData.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return authData;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return authData
-}, (error)=> {
-    return Promise.reject(error)
-})
+);
