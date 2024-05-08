65e8533a5589215e2d1fb5c9a7918677279cadd7
hayoung <wendy4332@naver.com>
2024-05-08 15:56:02 +0900

    feat:axiostInstance interceptor 추가

--- src/libs/axios/axiosInstance.ts
+++ src/libs/axios/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getSessionToken } from "../../utils/storageUtils";
 
 export const pocketmonInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API_POKETMON_BASE_URL}`,
@@ -6,4 +7,14 @@ export const pocketmonInstance = axios.create({
 
 export const userDataInstance = axios.create({
     baseURL:`${import.meta.env.VITE_APP_URL}`
+})
+
+userDataInstance.interceptors.request.use((authData)=>{
+    const token = getSessionToken()
+    if(token) {
+        authData.headers["Authorization"]=`Bearer ${token}`
+    }
+    return authData
+}, (error)=> {
+    return Promise.reject(error)
 })
