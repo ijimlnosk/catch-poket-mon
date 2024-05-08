e451ca7f1314a7316b9f9989d127f9fc9282ebec
hayoung <wendy4332@naver.com>
2024-05-08 21:51:33 +0900

    feat: dataApi 수정

--- src/libs/axios/dataAPI.ts
+++ src/libs/axios/dataAPI.ts
@@ -6,31 +6,47 @@ interface PokeRequest {
     name:string,
     url:string 
     background:string,
-
 }
+
+interface getPokeData {
+    apiKey:string,
+    auth:boolean,
+    pair:number
+}
+
+const defaultPokeData :getPokeData ={
+    apiKey:"mobi3rd1234",
+    auth:true,
+    pair:2
+}
+
 //포켓몬 포획 / 저장
-export const postData = async(postPokeData: PokeRequest ,auth:boolean=true)=>{
-    const postDataResponse = await userDataInstance.post("/data",postPokeData,{
+export const postData = async(postPokeData: PokeRequest )=>{
+    const postDataResponse = await userDataInstance.post('/data/poke',postPokeData,{
         params:{
-            auth:auth
+           defaultPokeData
         }
     })
     return postDataResponse.data
 }
 
 //포획한 포켓몬 불러오기
-export const getData = async(getPokeData:PokeRequest, auth:boolean=true)=>{
-    const getDataResponse= await userDataInstance.get("/data",{
+export const getData = async()=>{
+    const getDataResponse= await userDataInstance.get("/data/poke",{
         params:{
-            getPokeData,
-            auth:auth
+            defaultPokeData
         }
     })
+
     return getDataResponse.data
 }
 
 //포획된 포켓몬 놓아주기
-export const deleteData = async(deletePokeData:PokeRequest)=>{
-    const deleteDataResponse = await userDataInstance.delete("/data",deletePokeData)
+export const deleteData = async(id:number)=>{
+    const deleteDataResponse = await userDataInstance.delete(`/data/poke/${id}`,{
+        params:{
+            defaultPokeData
+        }
+    })
     return deleteDataResponse.data
 }
