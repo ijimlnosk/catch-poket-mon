846ed7ba4f35faa0f357d0edd07238756a82f7c7
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-10 03:07:46 +0900

    refactor: 기존 사용했던 modal 창 변경에 따른 수정

--- src/components/main/resultOverlay.tsx
+++ src/components/main/resultOverlay.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import Overlay from "../commons/overlay";
 import Modal from "../commons/modal";
 
 type ResultOverlayProps = {
@@ -12,17 +11,15 @@ const ResultOverlay = ({ result, onClose, onNavigate }: ResultOverlayProps) => {
     const navigate = useNavigate();
     const isSuccess = result === true;
     return (
-        <Overlay isOpen={result !== null} onClose={onClose}>
-            <Modal
-                title={isSuccess ? "포획 성공! 확인하러가기" : "포획 실패!"}
-                buttonText={isSuccess ? "다시하기" : "닫기"}
-                onClick={isSuccess ? onNavigate : onClose}
-                secondButtonText={isSuccess ? "가기" : ""}
-                onSecondClick={
-                    isSuccess ? () => navigate("/myinfo") : undefined
-                }
-            />
-        </Overlay>
+        <Modal
+            title={isSuccess ? "포획 성공! 확인하러가기" : "포획 실패!"}
+            buttonText={isSuccess ? "다시하기" : "닫기"}
+            onClick={isSuccess ? onNavigate : onClose}
+            secondButtonText={isSuccess ? "가기" : ""}
+            onSecondClick={isSuccess ? () => navigate("/myinfo") : undefined}
+            isOpen={result !== null}
+            onClose={onClose}
+        />
     );
 };
 export default ResultOverlay;
