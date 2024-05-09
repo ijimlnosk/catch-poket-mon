44db6298cb81bd8ed5ccd68f602c79719c1ba323
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-10 03:08:40 +0900

    refactor: 타입 고정으로 변경했습니다.

--- src/hook/useRandomPokeData.ts
+++ src/hook/useRandomPokeData.ts
@@ -1,17 +1,14 @@
 import { useQuery } from "react-query";
-import { AbilityRoot } from "../types/pokeTypes/abilityType";
 import { SpeciesRoot } from "../types/pokeTypes/speciesType";
 import { getAllPocketmon } from "../libs/axios/pokeAPI";
 import { PokemonRoot } from "../types/pokeTypes/pokemonType";
 
 interface PokeApiProps {
-    getAbilityInfo?: (pokeId: number) => Promise<AbilityRoot>;
-    getPokemonSpecies?: (pokeId: number) => Promise<SpeciesRoot>;
-    getPokemon?: (pokeId: number) => Promise<PokemonRoot>;
+    getPokemonSpecies: (pokeId: number) => Promise<SpeciesRoot>;
+    getPokemon: (pokeId: number) => Promise<PokemonRoot>;
 }
 
 export const useRandomPokeData = ({
-    getAbilityInfo,
     getPokemonSpecies,
     getPokemon,
 }: PokeApiProps) => {
@@ -24,14 +21,13 @@ export const useRandomPokeData = ({
                     const match = poke.url.match(/\/pokemon\/(\d+)\//);
                     if (match) {
                         const pokeId = parseInt(match[1], 10);
-                        const [info, species, pokemon] = await Promise.all([
-                            getAbilityInfo?.(pokeId) ?? null,
-                            getPokemonSpecies?.(pokeId) ?? null,
-                            getPokemon?.(pokeId) ?? null,
+                        const [species, pokemon] = await Promise.all([
+                            getPokemonSpecies(pokeId),
+                            getPokemon(pokeId),
                         ]);
 
-                        if (!info && !species && !pokemon) return null;
-                        return { info, species, pokemon };
+                        if (!species && !pokemon) return null;
+                        return { species, pokemon };
                     }
                     return null;
                 })
