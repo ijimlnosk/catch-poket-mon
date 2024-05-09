9e85c5121ae8d0705545223676ac468fca355596
hayoung <wendy4332@naver.com>
2024-05-10 04:06:23 +0900

    feat:signin,signup페이지 리펙토링 및 기능

--- src/pages/signinPage.tsx
+++ src/pages/signinPage.tsx
@@ -1,17 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-import logo from "../assets/imgs/logo.png";
-import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { Schema } from "../constants/schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useSigninMutation } from "../hook/useLogin";
-import Input from "../components/commons/input";
-import CustomButton from "../components/commons/button";
-
-interface FormValues {
-    userId: string;
-    password: string;
-}
+import SigninForm from "../components/signin/signinForm";
+import SigninScreen from "../components/signin/signinScreen";
 
 /**
  * signin page
@@ -19,29 +8,15 @@ interface FormValues {
  * @returns {}
  */
 
-const SigninPage: React.FC = () => {
+const SigninPage = () => {
     const [isOpen, setIsOpen] = useState<boolean>(true);
     const [showLoginForm, setShowLoginForm] = useState<boolean>(false);
     const transitionRef = useRef<HTMLDivElement>(null);
-    const navigate = useNavigate();
-    const mutation = useSigninMutation();
 
-    const { register, handleSubmit } = useForm<FormValues>({
-        resolver: zodResolver(Schema),
-    });
-
-    const onSubmit = (data: FormValues) => {
-        mutation.mutate(data);
-    };
     const handleSections = () => {
         setShowLoginForm(false);
         setIsOpen(!isOpen);
     };
-
-    const handleNavSignup = () => {
-        navigate("/signup");
-    };
-
     useEffect(() => {
         document.body.style.overflow = "hidden";
         return () => {
@@ -64,106 +39,14 @@ const SigninPage: React.FC = () => {
     }, [isOpen]);
 
     return (
-        <div className="w-full h-screen flex flex-col overflow-hidden">
-            {/* 빨간색 섹션 */}
-            <div
-                ref={transitionRef}
-                className={`w-full h-1/2 bg-COMMON-red flex flex-col justify-end items-center overflow-hidden transition-all duration-500 ease-in-out z-50 ${
-                    isOpen
-                        ? "transform translate-y-0"
-                        : "transform -translate-y-3/4"
-                }`}
-            >
-                <div className="flex w-full">
-                    <div className="w-[45%] flex flex-col justify-end items-center">
-                        <div className="w-full h-4 bg-SYSTEM-black "></div>
-                    </div>
-                    <div className="min-w-[310px] h-[160px]  bg-SYSTEM-black rounded-t-full flex flex-col justify-end items-center">
-                        <div className="w-60 h-32 bg-SYSTEM-white rounded-t-full flex flex-col justify-end items-center">
-                            <button
-                                onClick={handleSections}
-                                className="w-60 h-32  rounded-t-full hover:bg-COMMON-light-gray  transition-colors"
-                            ></button>
-                        </div>
-                    </div>
-                    <div className="w-[45%] flex flex-col justify-end items-center">
-                        <div className="w-full h-4 bg-SYSTEM-black "></div>
-                    </div>
-                </div>
-            </div>
-
-            <div
-                className={`flex items-center justify-center transition-opacity duration-500 ease-in-out absolute translate-x-[-50%] translate-y-[-50%] left-1/2 top-1/2 ${
-                    showLoginForm ? "opacity-100" : "opacity-0"
-                }`}
-            >
-                {/* signinForm */}
-                <div className="w-[400px]  flex items-center justify-center flex-col pb-20 rounded-xl">
-                    <div>
-                        <img src={logo} />
-                    </div>
-
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="flex items-center justify-center flex-col">
-                            <label className="pt-7">ID</label>
-                            <Input
-                                name="userId"
-                                placeholder="아이디를 입력하세요"
-                                className="border-2 border-COMMON-light-gray p-2 "
-                                register={register}
-                            />
-                            <label className="pt-7">Password</label>
-                            <Input
-                                name="password"
-                                type="password"
-                                placeholder="비밀번호를 입력하세요"
-                                className="border-2 border-COMMON-light-gray p-2 "
-                                register={register}
-                            />
-                            <div className="pt-[15px]">
-                                <CustomButton
-                                    variant="common"
-                                    size="small"
-                                    shape="roundedSquare"
-                                >
-                                    로그인
-                                </CustomButton>
-                            </div>
-                        </div>
-                    </form>
-
-                    <div className="flex flex-row">
-                        <p className="p-4 text-ti text-POKETYPE-rock">
-                            아직도 회원이 아니신가요?
-                        </p>
-                        <button onClick={handleNavSignup} className="text-ti">
-                            가입하러가기
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            {/* 하얀색 섹션 */}
-            <div
-                className={`w-full h-1/2 bg-SYSTEM-white flex flex-col justify-start items-center overflow-hidden transition-all duration-500 ease-in-out ${
-                    isOpen
-                        ? "transform translate-y-0"
-                        : "transform translate-y-3/4"
-                }`}
-            >
-                <div className="flex w-full">
-                    <div className="w-[45%]">
-                        <div className="w-full h-4 bg-SYSTEM-black"></div>
-                    </div>
-                    <div className="min-w-[310px] h-[160px] bg-SYSTEM-black rounded-b-full flex flex-col justify-start items-center">
-                        <div className="w-60 h-32 bg-SYSTEM-white rounded-b-full"></div>
-                    </div>
-                    <div className="w-[45%]">
-                        <div className="w-full h-4 bg-SYSTEM-black "></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <SigninScreen
+            isOpen={isOpen}
+            transitionRef={transitionRef}
+            handleSections={handleSections}
+            showLoginForm={showLoginForm}
+        >
+            <SigninForm />
+        </SigninScreen>
     );
 };
 export default SigninPage;
