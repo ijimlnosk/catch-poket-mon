79f380f31d4fa9b352505d58db3c09c0feb0e2ba
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-11 04:21:17 +0900

    refactor: 사이즈 수정

--- src/components/commons/pokemonCard.tsx
+++ src/components/commons/pokemonCard.tsx
@@ -1,24 +1,19 @@
-type PokeMon = {
-    pokeId: number;
-    type: string[];
-    name: string;
-    url: string;
-};
+import { PokemonData } from "../../types/pokeTypes/pokemonData";
 
-const PokemonCard = ({ pokeId, type, name, url }: PokeMon) => {
+const PokemonCard = ({ pokeId, type, name, url }: PokemonData) => {
     return (
         <div className="w-[200px] h-[200px] bg-SYSTEM-white flex justify-center items-center">
             <div className="w-[200px] h-[200px] flex justify-center items-center rounded-lg">
                 <div className="w-full flex justify-center items-center flex-col">
                     <div className="w-full h-[25px] flex justify-center items-center">
-                        <p className="text-[25px]">#{pokeId}</p>
+                        <p className="text-[20px]">#{pokeId}</p>
                     </div>
-                    <img src={url} className="w-[100px] h-[100px]" />
+                    <img src={url} className="w-[80px] h-[80px]" />
                     <div className="w-full flex justify-center items-center flex-col ">
                         <div className="w-full rounded-xl flex justify-center items-center">
                             {type.map((poke, idx) => (
-                                <div className="border-2 rounded-lg">
-                                    <p key={idx}>{poke}</p>
+                                <div key={idx} className="border-2  rounded-lg">
+                                    <p>{poke}</p>
                                 </div>
                             ))}
                         </div>
