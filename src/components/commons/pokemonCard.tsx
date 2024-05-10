e35a9389d53a0d94db3d6643d0d7ff8581e3b275
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-10 16:50:59 +0900

    design: css 수정했습니다

--- src/components/commons/pokemonCard.tsx
+++ src/components/commons/pokemonCard.tsx
@@ -8,22 +8,23 @@ type PokeMon = {
 
 const PokemonCard = ({ pokeId, type, name, url, background }: PokeMon) => {
     console.log(background);
-
     return (
-        <div className="">
-            <div className="">
-                <div>
-                    <p>#{pokeId}</p>
-                </div>
-                <img src={url} />
-                <div>
-                    <div>
-                        {type.map((poke, idx) => (
-                            <p key={idx}>{poke}</p>
-                        ))}
+        <div className="w-[300px] h-[300px] bg-SYSTEM-white flex justify-center items-center">
+            <div className="w-[250px] h-[250px] flex justify-center items-center rounded-lg bg-COMMON-dark_gray">
+                <div className="w-full flex justify-center items-center flex-col">
+                    <div className="w-full h-[25px] flex justify-center items-center">
+                        <p className="text-[25px]">#{pokeId}</p>
                     </div>
-                    <div>
-                        <p>{name}</p>
+                    <img src={url} className="w-[160px] h-[160px]" />
+                    <div className="w-full flex justify-center items-center flex-col ">
+                        <div className="w-full rounded-xl flex justify-center items-center">
+                            {type.map((poke, idx) => (
+                                <p key={idx}>{poke}</p>
+                            ))}
+                        </div>
+                        <div className="w-full flex justify-center items-center">
+                            <p>{name}</p>
+                        </div>
                     </div>
                 </div>
             </div>
