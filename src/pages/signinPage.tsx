04bb1f90c9ff3824b3a4b688ba9a8f3f6ef5a11b
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-10 17:33:38 +0900

    refactor: 불필요한 로직 삭제

--- src/pages/signinPage.tsx
+++ src/pages/signinPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SigninForm from "../components/signin/signinForm";
 import SigninScreen from "../components/signin/signinScreen";
 
