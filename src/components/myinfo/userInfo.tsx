4eadec6dc128974f1f388c9c7acca34bdbd732fc
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-12 04:49:44 +0900

    feat: 닉네임 수정 버튼 생성

--- src/components/myinfo/userInfo.tsx
+++ src/components/myinfo/userInfo.tsx
@@ -1,24 +1,63 @@
+import { useState } from "react";
+import CustomButton from "../commons/button";
+import { useMutation } from "react-query";
+import { patchUpdateInfo } from "../../libs/axios/userAPI";
+import LogoutButton from "./logoutButton";
+
 type userInfo = {
     nickName: string;
     userId: string;
 };
 
 const UserInfo = ({ nickName, userId }: userInfo) => {
+    const [editMode, setEditMode] = useState(false);
+    const [updateNickname, setUpdateNickName] = useState(nickName || "");
+
+    const updateMutation = useMutation(patchUpdateInfo, {
+        onSuccess: () => {
+            setEditMode(false);
+        },
+    });
+
+    const handleEditClick = () => {
+        setEditMode(true);
+    };
+    const handleSaveClick = () => {
+        updateMutation.mutate({ nickName: updateNickname });
+    };
+
     return (
         <div className="w-[600px] h-[300px] bg-SYSTEM-white ml-8 rounded-xl flex justify-center items-center flex-col p-4">
             <p className="text-lg pb-4">회원정보</p>
             <div className="w-[90%] h-[200px] border-2 border-COMMON-light_gray rounded-xl flex flex-col justify-center">
                 <div className="w-full flex flex-row  justify-start items-center pt-4">
                     <p className="text-lg pl-4">닉네임 :</p>
-                    <p className="w-[300px] text-lg text-center">{nickName}</p>
-                    <button className="">수정</button>
+                    {editMode ? (
+                        <input
+                            type="text"
+                            className="w-[300px] text-lg text-center"
+                            value={updateNickname}
+                            onChange={(e) => setUpdateNickName(e.target.value)}
+                        />
+                    ) : (
+                        <p className="w-[300px] text-lg text-center">
+                            {nickName}
+                        </p>
+                    )}
+                    <CustomButton
+                        variant={"common"}
+                        size={"small"}
+                        onClick={editMode ? handleSaveClick : handleEditClick}
+                    >
+                        {editMode ? "완료" : "수정"}
+                    </CustomButton>
                 </div>
                 <div className="w-full flex flex-row  justify-start items-center pt-4">
                     <p className="text-lg pl-4">아이디 :</p>
                     <p className="w-[300px] text-lg text-center">{userId}</p>
                 </div>
                 <div className="w-full flex flex-row  justify-center items-center pt-4">
-                    <button className="w-[184px] text-lg p-2">로그아웃</button>
+                    <LogoutButton />
                 </div>
             </div>
         </div>
