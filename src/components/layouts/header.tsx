97edb1632f869f1a29d1de8076b26a0590cd00e4
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-16 01:40:57 +0900

    refactor : 로고 클릭 시 메인 페이지로 이동

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/imgs/logo.png";
 import monsterball from "../../assets/imgs/monsterball.png";
+import { useQueryClient } from "react-query";
 
 /**
  * header
@@ -10,12 +11,21 @@ import monsterball from "../../assets/imgs/monsterball.png";
 
 const Header = () => {
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
     const onMoveMyInfo = () => {
         navigate("/myInfo");
+        queryClient.invalidateQueries("pokemon");
+    };
+    const onClickLogo = () => {
+        navigate("/");
+        queryClient.invalidateQueries("pokeData");
     };
     return (
         <header className="flex items-center justify-between p-4  bg-SYSTEM-whte h-[140px] w-[100%]">
-            <div className="flex items-center space-x-4 flex-grow">
+            <div
+                className="flex items-center space-x-4 flex-grow cursor-pointer"
+                onClick={onClickLogo}
+            >
                 <img src={monsterball} className="h-12" />
                 <img src={logo} className="h-12" />
             </div>
