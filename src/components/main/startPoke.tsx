642065409d16012d8d7b29b0bfe75db5543b9c47
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-16 00:45:47 +0900

    refactor : 자막 배너 반응형 설정

--- src/components/main/startPoke.tsx
+++ src/components/main/startPoke.tsx
@@ -27,11 +27,11 @@ const StartPoke = ({ onGameStart }: GameStartProps) => {
     return (
         <>
             {gameStart ? (
-                <div className="flex flex-col justify-center items-center">
+                <div className="w-full flex flex-col justify-center items-center">
                     <div className="w-full flex items-center justify-center">
-                        <div className="w-[922px] h-[700px] flex justify-center items-center flex-col ">
+                        <div className="w-full h-[700px] flex justify-center items-center flex-col ">
                             <img src={unknownPoketMon} className="h-[600px]" />
-                            <div className="w-[100%] h-[50px] mt-[-10px] text-xl bg-SYSTEM-white text-center  border border-SYSTEM-black rounded-md">
+                            <div className="max-w-[922px] w-[100%] h-[100px] mt-[-10px] text-xl bg-SYSTEM-white text-center  border border-SYSTEM-black rounded-md sm:max-h-[50px]">
                                 앗! 야생 포켓몬이 나타났다!
                             </div>
                         </div>
