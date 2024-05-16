c9203a7b5f9f4992985953d44055558e15968cf2
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-16 21:57:10 +0900

    refactor : userInfo error 모달 처리

--- src/components/myinfo/userInfo.tsx
+++ src/components/myinfo/userInfo.tsx
@@ -4,6 +4,7 @@ import { useMutation } from "react-query";
 import { patchUpdateInfo } from "../../libs/axios/userAPI";
 import LogoutButton from "./logoutButton";
 import { setSessionUserInfo } from "../../utils/storageUtils";
+import Modal from "../commons/modal";
 
 type userInfo = {
     nickName: string;
@@ -13,6 +14,7 @@ type userInfo = {
 const UserInfo = ({ nickName, userId }: userInfo) => {
     const [editMode, setEditMode] = useState(false);
     const [updateNickname, setUpdateNickName] = useState(nickName || "");
+    const [isError, setIsError] = useState(false);
 
     const updateMutation = useMutation(patchUpdateInfo, {
         onSuccess: () => {
@@ -20,8 +22,8 @@ const UserInfo = ({ nickName, userId }: userInfo) => {
             window.location.reload();
             setEditMode(false);
         },
-        onError: (error) => {
-            console.error(error);
+        onError: () => {
+            setIsError(true);
         },
     });
 
@@ -38,40 +40,54 @@ const UserInfo = ({ nickName, userId }: userInfo) => {
     };
 
     return (
-        <div className="w-[600px] h-[300px] bg-SYSTEM-white ml-8 rounded-xl flex justify-center items-center flex-col p-4">
-            <p className="text-lg pb-4">회원정보</p>
-            <div className="w-[90%] h-[200px] border-2 border-COMMON-light_gray rounded-xl flex flex-col justify-center">
-                <div className="w-full flex flex-row  justify-start items-center pt-4">
-                    <p className="text-lg pl-4">닉네임 :</p>
-                    {editMode ? (
-                        <input
-                            type="text"
-                            className="w-[300px] text-lg text-center border-2 rounded-lg"
-                            value={updateNickname}
-                            onChange={(e) => setUpdateNickName(e.target.value)}
-                        />
-                    ) : (
+        <>
+            <Modal
+                title="에러가 발생했습니다"
+                isOpen={isError}
+                onClose={() => setIsError(false)}
+                buttonText="확인"
+            />
+            <div className="w-[600px] h-[300px] bg-SYSTEM-white ml-8 rounded-xl flex justify-center items-center flex-col p-4">
+                <p className="text-lg pb-4">회원정보</p>
+                <div className="w-[90%] h-[200px] border-2 border-COMMON-light_gray rounded-xl flex flex-col justify-center">
+                    <div className="w-full flex flex-row  justify-start items-center pt-4">
+                        <p className="text-lg pl-4">닉네임 :</p>
+                        {editMode ? (
+                            <input
+                                type="text"
+                                className="w-[300px] text-lg text-center border-2 rounded-lg"
+                                value={updateNickname}
+                                onChange={(e) =>
+                                    setUpdateNickName(e.target.value)
+                                }
+                            />
+                        ) : (
+                            <p className="w-[300px] text-lg text-center">
+                                {nickName}
+                            </p>
+                        )}
+                        <CustomButton
+                            variant={"common"}
+                            size={"small"}
+                            onClick={
+                                editMode ? handleSaveClick : handleEditClick
+                            }
+                        >
+                            {editMode ? "완료" : "수정"}
+                        </CustomButton>
+                    </div>
+                    <div className="w-full flex flex-row  justify-start items-center pt-4">
+                        <p className="text-lg pl-4">아이디 :</p>
                         <p className="w-[300px] text-lg text-center">
-                            {nickName}
+                            {userId}
                         </p>
-                    )}
-                    <CustomButton
-                        variant={"common"}
-                        size={"small"}
-                        onClick={editMode ? handleSaveClick : handleEditClick}
-                    >
-                        {editMode ? "완료" : "수정"}
-                    </CustomButton>
-                </div>
-                <div className="w-full flex flex-row  justify-start items-center pt-4">
-                    <p className="text-lg pl-4">아이디 :</p>
-                    <p className="w-[300px] text-lg text-center">{userId}</p>
-                </div>
-                <div className="w-full flex flex-row  justify-center items-center pt-4">
-                    <LogoutButton />
+                    </div>
+                    <div className="w-full flex flex-row  justify-center items-center pt-4">
+                        <LogoutButton />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 export default UserInfo;
