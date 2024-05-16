bbb6a7c7380076029dba692d13be4a0baa6fc6f9
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-16 18:02:43 +0900

    refactor: 로그아웃 추가

--- src/components/myinfo/logoutButton.tsx
+++ src/components/myinfo/logoutButton.tsx
@@ -1,11 +1,13 @@
 import { useMutation } from "react-query";
 import CustomButton from "../commons/button";
 import { postSignout } from "../../libs/axios/userAPI";
+import { useNavigate } from "react-router-dom";
 
 const LogoutButton = () => {
+    const navigate = useNavigate();
     const logoutMutation = useMutation(postSignout, {
         onSuccess: () => {
-            window.location.href = "/signin";
+            navigate("/signin");
         },
         onError: (error) => {
             console.error("로그아웃 실패:", error);
