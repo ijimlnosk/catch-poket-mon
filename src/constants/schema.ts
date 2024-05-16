ee42465fe6a212263359a61df9eae5a194f1d2c1
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-05-16 18:02:14 +0900

    refactor : zod nickname 추가

--- src/constants/schema.ts
+++ src/constants/schema.ts
@@ -10,6 +10,7 @@ export const SigninSchema = z.object({
         .min(8)
         .optional(),
 });
+
 export const SignupSchema = z.object({
     userId: z.string().min(1, { message: "ID를 입력해주세요" }).optional(),
     password: z
@@ -19,4 +20,10 @@ export const SignupSchema = z.object({
         })
         .min(8)
         .optional(),
+    data: z.object({
+        nickName: z
+            .string()
+            .min(1, { message: "닉네임을 입력해주세요" })
+            .max(6, { message: "최대 6글자까지 입력 가능합니다" }),
+    }),
 });
